a181785cd2441edc3af010bad4bad990c09ce506
Haseenasikkandar <haseenasikkandartce@gmail.com>
2017-03-21 12:07:46 +0530

    Update subset.c

--- src/subset.c
+++ src/subset.c
@@ -87,7 +87,7 @@ void EvalSubset(Attribute Att, CaseCount Cases)
     CaseCount	KnownCases;
     Boolean	Better;
 	int i=0;
-double alpha =1.95;
+double alpha =1.50;
 double q= 1/(1-alpha);
     /*  First compute Freq[][], ValFreq[], base info, and the gain
 	and total info of a split on discrete attribute Att  */
@@ -376,7 +376,7 @@ void Merge(DiscrValue x, DiscrValue y, CaseCount Cases)
     double	Entr=0;
     CaseCount	KnownCases=0;
     int		R, C;
-double alpha =1.95;
+double alpha =1.50;
 	int i=0;
 	double q=1/(1-alpha);
 	double count[20];
@@ -455,7 +455,7 @@ void EvaluatePair(DiscrValue x, DiscrValue y, CaseCount Cases)
     ClassNo	c;
     double	Entr=0;
     CaseCount	KnownCases=0, F;
-double alpha=1.95;
+double alpha=1.50;
 	int i=0;
 	double count[20];
 double q = 1/(1-alpha);
